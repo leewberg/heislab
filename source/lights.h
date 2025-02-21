78c9bdcd8ec5466a1a695c3cb4686ca16acb4002
Lee Wesenberg <lee@Lee-sin-Laptop.gmail.com>
2025-02-21 08:17:21 +0100

    added more basic classes

---
+++ source/lights.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include "driver/elevio.h"
+
+void lightIndicator(int number);
+
+void extInd(int number);
---
+++ source/timer.c
@@ -0,0 +1,5 @@
+#include "timer.h"
+
+void timer(int time){
+    printf("Here, we will stall the program based on the int");
+}
